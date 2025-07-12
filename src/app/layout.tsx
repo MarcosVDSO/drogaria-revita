c181dbe7242a8b942174e68dd44132f83b219031
Marcos Oliveira <95980171+MarcosVDSO@users.noreply.github.com>
2025-07-12 19:24:12 -0300

    fix: troca de favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Drogaria Revita - Confiança e economia para sua saúde",
+  title: "Drogaria Revita",
   description: "A farmácia de confiança do Jardim América. Atendimento carismático, entrega rápida e os melhores preços de São Luís.",
+  icons: {
+    icon: "/icone.jpg", // This path refers to the /public directory
+  },
 };
 
 export default function RootLayout({
@@ -17,6 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <Head>
+        <link rel="icon" href="/icone.jpg" />
+      </Head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
