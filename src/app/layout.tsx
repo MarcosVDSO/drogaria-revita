1bdd3192d7f83d2769143888ba8110badcb308d2
gbestrategiaerastreamento-eng <gbestrategiaerastreamento@gmail.com>
2026-05-08 23:27:27 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,16 @@
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import Head from "next/head";
 import Script from "next/script";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Drogaria Revita",
-  description: "A farmácia de confiança do Jardim América. Atendimento carismático, entrega rápida e os melhores preços de São Luís.",
+  description:
+    "A farmácia de confiança do Jardim América. Atendimento carismático, entrega rápida e os melhores preços de São Luís.",
   icons: {
-    icon: "/icone.jpg", // This path refers to the /public directory
+    icon: "/icone.jpg",
   },
 };
 
@@ -22,25 +21,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <Head>
-          <!-- Google Tag Manager -->
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WNR9T4DH');</script>
-          <!-- End Google Tag Manager -->
-        
-        <link rel="icon" href="/icone.jpg" />
-      </Head>
-
       <body className={inter.className}>
-          <!-- Google Tag Manager (noscript) -->
-            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WNR9T4DH"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-          <!-- End Google Tag Manager (noscript) -->
-        {children}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer' ? '&l='+l : '';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WNR9T4DH');
+          `}
+        </Script>
 
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WNR9T4DH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {children}
       </body>
     </html>
   );
